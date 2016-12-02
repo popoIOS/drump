52b95c74e8e175e3b017b2921e4fa8f7e267a8eb
贾应许 <iphone_project@163.com>
2016-12-02 10:06:24 +0800

    Initial Commit

---
+++ Drump/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Drump
+//
+//  Created by ydz on 16/12/2.
+//  Copyright © 2016年 JYX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Drump/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Drump
+//
+//  Created by ydz on 16/12/2.
+//  Copyright © 2016年 JYX. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
